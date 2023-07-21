7b13e99e791b69282caffa9c4c52aa8279ac3efe
Rafael Borges <rafael.borges2698@gmail.com>
2023-07-21 16:07:48 -0300

    added experiences cards

--- src/components/Organisms/Experiences/mock.ts
+++ src/components/Organisms/Experiences/mock.ts
@@ -1,6 +1,9 @@
 import IconBmg from '@/assets/icons/bmg.svg'
 import IconGetnet from '@/assets/icons/getnet.svg'
 import IconPopeyes from '@/assets/icons/popeyes.svg'
+import IconPagonxt from '@/assets/icons/pagonxt.svg'
+import IconBuy from '@/assets/icons/buy.svg'
+import IconGoBarber from '@/assets/icons/goBarber.svg'
 
 export const experiences = [{
     id: 0,
@@ -28,33 +31,25 @@ export const experiences = [{
 },
 {
     id: 3,
-    title: 'Banco Bmg',
-    description: 'Web application of financial products for a Brazil bank.',
-    labels: ['Typescript', 'React', 'Next.js', 'Node.js'],
-    logo: IconBmg,
-    logoDescription: 'Bmg'
+    title: 'PagoNxt',
+    description: `A platform that aims to serve Getnet's global and local operators.`,
+    labels: ['Typescript', 'React', 'Design System'],
+    logo: IconPagonxt,
+    logoDescription: 'PagoNxt'
 },
 {
-    id: 3,
-    title: 'Banco Bmg',
-    description: 'Web application of financial products for a Brazil bank.',
-    labels: ['Typescript', 'React', 'Next.js', 'Node.js'],
-    logo: IconBmg,
-    logoDescription: 'Bmg'
+    id: 4,
+    title: 'Buy Management',
+    description: 'The project is a shopping manager with some functionalities to help the store, with personalized functionalities for the customer.',
+    labels: ['Typescript', 'React', 'Firebase'],
+    logo: IconBuy,
+    logoDescription: 'Buy Management'
 },
 {
-    id: 3,
-    title: 'Banco Bmg',
-    description: 'Web application of financial products for a Brazil bank.',
-    labels: ['Typescript', 'React', 'Next.js', 'Node.js'],
-    logo: IconBmg,
-    logoDescription: 'Bmg'
-},
-{
-    id: 3,
-    title: 'Banco Bmg',
-    description: 'Web application of financial products for a Brazil bank.',
-    labels: ['Typescript', 'React', 'Next.js', 'Node.js'],
-    logo: IconBmg,
-    logoDescription: 'Bmg'
+    id: 5,
+    title: 'GoBarber',
+    description: `Project for hairdressers to manage their schedules. Following the course of rocketseat's bootcamp`,
+    labels: ['Typescript', 'React', 'Node.js', 'Jest'],
+    logo: IconGoBarber,
+    logoDescription: 'GoBarber'
 }]
