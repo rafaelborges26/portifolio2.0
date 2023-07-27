dc357e1ead6658bb1e8516f1e60233e571b7925f
Rafael Borges <rafael.borges2698@gmail.com>
2023-07-27 18:34:15 -0300

    added metatags to title and description of seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Sora:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
+      <meta charSet="UTF-8" />
+      <meta name="description" content="Hello! I´m a Brazilian software engineer"/>
+
+      <meta property="og:title" content="Rafael Borges" />
+      <meta property="og:description" content="Hello! I´m a Brazilian software engineer"/>
+      <meta property="og:url" content="https://rafaelborges.dev.br/" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Sora:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
       </Head>
       <body>
         <Main />
